4c4fd946a6de021390a62517956fffc803926fda
DimasAgung <dimasraden0798@gmail.com>
2022-03-18 16:01:55 +0800

    Menambahkan Function ShowResult untuk console

--- src/assets/scripts/script.js
+++ src/assets/scripts/script.js
@@ -6,6 +6,28 @@ const CONDITION_TABLE = [
 ]
 // "D" = Draw, "P" = Player 1 win, "C" = Com win
 
+//Player cant Input Again After Choose The Option
+const disableInput = () => {
+    document.querySelectorAll(".figure").forEach(input => {
+        input.setAttribute("disabled", "disabled")
+    })
+}
+
+//Show Game Result
+const showResult = result => {
+    const statusResult = {
+        "D": ["Result : Draw!", "draw"],
+        "P": ["Result : Player 1 win!", "player-win"],
+        "C": ["Result : Com win!", "com-win"]
+    }
+
+    console.log(statusResult[result][0]);
+    document.getElementById("versus").style.display = "none"; //Hide Versus on Browser
+    document.getElementById(statusResult[result][1]).style.display = "block"
+    disableInput()
+}
+
+
 //Player 1 & COM CHOICE
 const getChoice = player_choice => {
     const PLAYER_CHOICE = ["rock", "paper", "scissors"];
@@ -17,4 +39,16 @@ const getChoice = player_choice => {
     //TEXT CONSOLE COM RANDOM RESULT
     const com_choice = Math.floor(Math.random() * 3); //Untuk merandom pilihan com
     console.log("COM Choose :", COM_CHOICE[com_choice]);
+
+    //DISPLAY PLAYER CHOISE WITH BORDER
+    document.getElementById(PLAYER_CHOICE[player_choice]).classList.add("border-pick");
+    document.getElementById(COM_CHOICE[com_choice]).classList.add("border-pick");
+    document.querySelectorAll(".figure").forEach(player_choice => {
+        player_choice.classList.remove("player-box");
+    })
+
+    //Game Result
+    const result = CONDITION_TABLE[player_choice][com_choice];
+    //To Show The Result 
+    showResult(result);
 }
