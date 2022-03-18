b9bc2736ff269d40d408499476ec157b989596b2
DimasAgung <dimasraden0798@gmail.com>
2022-03-18 15:10:15 +0800

    Bisa Menampilkan Pilihan COM di Console Browser

--- src/assets/scripts/script.js
+++ src/assets/scripts/script.js
@@ -11,6 +11,10 @@ const getChoice = player_choice => {
     const PLAYER_CHOICE = ["rock", "paper", "scissors"];
     const COM_CHOICE = ["com-rock", "com-paper", "com-scissors"];
 
-    //TEXT CONSOLE RESULT
+    //TEXT CONSOLE PLAYER CHOOSE RESULT
     console.log("Player 1 Choose :", PLAYER_CHOICE[player_choice]);
-};
+
+    //TEXT CONSOLE COM RANDOM RESULT
+    const com_choice = Math.floor(Math.random() * 3); //Untuk merandom pilihan com
+    console.log("COM Choose :", COM_CHOICE[com_choice]);
+}
